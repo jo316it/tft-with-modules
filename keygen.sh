f409f89b5eebce42c61ce23d023b816015efda65
jo316it <ricardofromit@gmail.com>
2023-01-10 12:42:42 -0300

    change2

--- keygen.sh
+++ keygen.sh
@@ -6,6 +6,8 @@ read KEY
 if [[ "${KEY}" =~ [^A-Za-z0-9\&./=_:?-] ]]; then
     echo "That NAME is NOT allowed."
 else
+    mkdir -p ./modules/ec2/keys
+    sleep 2
     ssh-keygen -f ./modules/ec2/keys/$KEY  -q -P ""
     echo "Ok the key was created..."
     echo "Connect from this directory using the command 'ssh -i $KEY user@ip.from.aws'"
